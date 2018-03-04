b2adf4348e8af411e3e9f77a94f9186ccc8eba17
Matthew McCorry <mccorry@gmail.com>
2018-03-04 20:09:15 +0000

    Updated deloy script

--- buildAndDeploy.fsx
+++ buildAndDeploy.fsx
@@ -8,28 +8,25 @@ build.WaitForExit ()
 
 match build.ExitCode with
 | 0 -> 
-    let portName = "COM3"
+    let firstPort = System.IO.Ports.SerialPort.GetPortNames() |> Seq.head
+    let portName = firstPort
     let baud = 115200
     System.Console.WriteLine "Build Success"
     let script = System.IO.File.ReadAllText "./out/bundle.js"
-    use port = new System.IO.Ports.SerialPort (portName, baud)
+    use port = new System.IO.Ports.SerialPort (portName, baud, System.IO.Ports.Parity.None, 8, System.IO.Ports.StopBits.One)
+    port.DtrEnable <- true
+    port.RtsEnable <- true
     port.Open ()
+    port.WriteLine("reset();")
+    port.ReadLine () |> ignore
     port.WriteLine script
     port.BaseStream.Flush ()
-    //port.DiscardInBuffer ()
-    //port.Close ()
 
     port.ReadExisting () |> ignore
-    //System.Threading.Thread.Sleep 100
 
-    
     System.Console.WriteLine "Hit ctrl C to quit"
-    // use listenPort = new System.IO.Ports.SerialPort (portName, baud)
-    // listenPort.Open ()
-    // listenPort.Close ()
     while true do
         System.Threading.Thread.Sleep 500
         System.Console.Write ( port.ReadExisting ())
     
-| _ -> 
-    System.Console.WriteLine "Build Fail"
+| _ -> System.Console.WriteLine "Build Fail"
