9564c2e2a7023bb998efc9eb8eff96421c400b3e
Matthew McCorry <mccorry@gmail.com>
2018-03-21 00:50:29 +0000

    cleaned build script

--- buildAndDeploy.fsx
+++ buildAndDeploy.fsx
@@ -8,25 +8,26 @@ build.WaitForExit ()
 
 match build.ExitCode with
 | 0 -> 
-    let firstPort = System.IO.Ports.SerialPort.GetPortNames() |> Seq.head
-    let portName = firstPort
-    let baud = 115200
-    System.Console.WriteLine "Build Success"
-    let script = System.IO.File.ReadAllText "./out/bundle.js"
-    use port = new System.IO.Ports.SerialPort (portName, baud, System.IO.Ports.Parity.None, 8, System.IO.Ports.StopBits.One)
-    port.DtrEnable <- true
-    port.RtsEnable <- true
-    port.Open ()
-    port.WriteLine("reset();")
-    port.ReadLine () |> ignore
-    port.WriteLine script
-    port.BaseStream.Flush ()
+    let firstPort = System.IO.Ports.SerialPort.GetPortNames() |> Seq.tryHead
+    match firstPort with
+    | Some port -> 
+        let baud = 115200
+        System.Console.WriteLine "Build Success"
+        let script = System.IO.File.ReadAllText "./out/bundle.js"
+        use port = new System.IO.Ports.SerialPort (port, baud, System.IO.Ports.Parity.None, 8, System.IO.Ports.StopBits.One)
+        port.DtrEnable <- true
+        port.RtsEnable <- true
+        port.Open ()
+        port.WriteLine("reset();")
+        port.ReadLine () |> ignore
+        port.WriteLine script
+        port.BaseStream.Flush ()
 
-    port.ReadExisting () |> ignore
+        port.ReadExisting () |> ignore
 
-    System.Console.WriteLine "Hit ctrl C to quit"
-    while true do
-        System.Threading.Thread.Sleep 500
-        System.Console.Write ( port.ReadExisting ())
-    
+        System.Console.WriteLine "Hit ctrl C to quit"
+        while true do
+            System.Threading.Thread.Sleep 500
+            System.Console.Write ( port.ReadExisting ())
+    | None -> System.Console.WriteLine "No serial port found"
 | _ -> System.Console.WriteLine "Build Fail"
